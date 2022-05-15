a51f79e1a75dced6808d07fce78bb9e74125f78b
Stacey Csikos <stacey@Staceys-MBP.lan>
2022-05-14 22:02:53 -0400

    added resume

--- stayseecheekosh/src/Components/Nabar/Navbar.jsx
+++ stayseecheekosh/src/Components/Nabar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
+import pdf from '../documents/staceycsikosresume.pdf'
 
 export default function Navbar() {
   return (
@@ -10,6 +11,7 @@ export default function Navbar() {
         <Link to="/about"><button className='btn'>About</button></Link>
         <Link to="/projects"><button className='btn'>Projects</button></Link>
         <Link to="/contact"><button className='btn'>Contact</button></Link>
+        <a href = {pdf} target = "_blank"><button className="btn">Resume</button></a>
       </div>
     </div>
   )
