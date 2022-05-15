81e18b0b0ccd8621cf890a0861cb929daa3a1317
Stacey Csikos <stacey@Staceys-MBP.lan>
2022-05-14 22:25:18 -0400

    updated

--- stayseecheekosh/src/Components/About/About.jsx
+++ stayseecheekosh/src/Components/About/About.jsx
@@ -9,14 +9,17 @@ export default function About() {
       <img className='picture' src={Image} />
 
       <p className='intro'>Hi there! My name is Stacey Csikos (Stay-see Cheek-osh).
-        I'm a full stack developer. I really enjoy building
+        I'm a Full-Stack Developer with 6+ years of experience in the service industry.
+        I really enjoy building
         back-ends because I feel they are the foundation to websites. With a strong foundation we
         can create really great websites for users to enjoy. I like the creativity
         and different challenges that come when developing a front-end.
-        On my free time I like exploring trails, traveling the tri-state,
-        and top rope climbing. Part of New York City since I was born.  
-        I'm used to learning at a fast pace and working efficiently, which works really
-        well with coding. 
+        Part of New York City since I was born. I'm used to learning at a fast pace and working efficiently, which works really
+        well with coding. I'm constantly chasing the feeling of happiness I get from accomplishments.
+        I find joy in calming the waves created by problems and even more joy when finding the solution.
+        As a rock climber I have to think about my next move and challenge myself to get there.
+        As a developer I'm faced with an ever changing environment that allows for more solutions
+        that I can find collaboratively. 
       </p>
     </div>
   )
--- stayseecheekosh/src/Components/Contact/Contact.jsx
+++ stayseecheekosh/src/Components/Contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './contact.css'
 import emailjs from 'emailjs-com'
+import github from '../../images/github.png'
+import linkedin from '../../images/linkedin.png'
 
 export default function Contact() {
 
@@ -18,7 +20,14 @@ export default function Contact() {
 
   return (
     <div className='div-contact'>
-   
+      <h2 className='or'>Find me on theese platforms:</h2>
+      <div className='social'>
+        <a href="https://github.com/staceycsikos/" target="_blank">
+          <img src={github} className="gitty" /></a>
+        <a href="https://www.linkedin.com/in/staceycsikos/" target="_blank">
+          <img src={linkedin} className="linky" /></a>
+      </div>
+      <h2 className='or'>Email me</h2>
       <a className='email-button' href='mailto:staceycsikos@icloud.com' target="_blank">
        <button className='send pop'>Pop up your preffered email application </button>
       </a>
