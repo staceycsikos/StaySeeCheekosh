4daa561930191ede9e41da87a0a456826b1336f0
Stacey Csikos <stacey@staceys-mbp.myfiosgateway.com>
2022-05-24 22:51:40 -0400

    minor styling changes and made home page more dynamic

--- stayseecheekosh/src/Screen/Home.jsx
+++ stayseecheekosh/src/Screen/Home.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './home.css'
-import hanoi from '../images/hanoi.png'
-import peakscout from '../images/peakscout.png'
-import movie from '../images/movie.png'
 import { projectData } from '../project'
 
 
@@ -14,28 +11,18 @@ export default function Home() {
     <div>
       <h3 className='intro home-intro'>Hi there! My name is Stacey Csikos and I like to create websites </h3>
       <div className='project-container'>
-
-        <Link to='/project/hanoi' projectData= {projectData[0]}>
-          <img className='pro' src={hanoi} title='Tower Of Hanoi' />
-          <h3 className='intro project-intro'>
-            Tower of Hanoi
-          </h3>
-        </Link>
-    
-        <Link to='/project/peakscout'>
-          <img className='pro' src={peakscout} title='Peak Scout' />
-          <h3 className='intro project-intro'>
-            Peak Scout
-          </h3>
-        </Link>
-        
-        <Link to='/project/7kmovies'>
-          <img className='pro' src={movie} title='7k Movies' />
-          <h3 className='intro project-intro'>
-            7k Movies
-          </h3>
-        </Link>
-
+        {projectData.map((project) => {
+          return (
+            <div>
+              <Link to={`/project/${project.id}`}>
+                <img className='pro' src={project.image} title={project.title} />
+                <h3 className='intro project-intro'>
+                  {project.title}
+                </h3>
+              </Link>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
